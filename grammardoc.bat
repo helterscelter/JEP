5c03b12dfeec1bc5b091b72df5f8972fe701c282
nathanfunk <nathanfunk@216fd33d-161a-0410-b2e1-d58788b044ed>
2002-05-25 23:18:58 +0000

    added mkdir of doc and doc\grammar
    git-svn-id: https://jep.svn.sourceforge.net/svnroot/jep/trunk/jep@9 216fd33d-161a-0410-b2e1-d58788b044ed

--- grammardoc.bat
+++ grammardoc.bat
@@ -1,3 +1,5 @@
+mkdir doc
+mkdir doc\grammar
 call jjdoc -OUTPUT_FILE:doc\grammar\grammar.html src\org\nfunk\jep\Parser.jj
 call jjdoc -TEXT:true -OUTPUT_FILE:doc\grammar\grammar.txt src\org\nfunk\jep\Parser.jj
 pause
