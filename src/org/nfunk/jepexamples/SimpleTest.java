649c93444d713af377a3b9ea9c1df01400275d71
nathanfunk <nathanfunk@216fd33d-161a-0410-b2e1-d58788b044ed>
2002-06-06 17:37:49 +0000

    A simple testing class for the basic accessibility of JEP
    git-svn-id: https://jep.svn.sourceforge.net/svnroot/jep/trunk/jep@26 216fd33d-161a-0410-b2e1-d58788b044ed

---
+++ src/org/nfunk/jepexamples/SimpleTest.java
@@ -0,0 +1,15 @@
+import org.nfunk.jep.*;
+
+/**
+ * A seven line program for testing whether the JEP library can be found
+ * by the compiler and at run-time.<br>
+ * Upon successful compilation and running of the program, the program should
+ * print out one line: "1+2 = 3.0"
+ */
+public class SimpleTest {
+		public static void main(String args[]) {
+				JEP myParser = new JEP();
+				myParser.parseExpression("1+2");
+				System.out.println("1+2 = " + myParser.getValue());
+		}	
+}
