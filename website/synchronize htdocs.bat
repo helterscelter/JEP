f64d39a3240dbfce8acf8e394bb5c60f02582404
nathanfunk <nathanfunk@216fd33d-161a-0410-b2e1-d58788b044ed>
2002-07-11 05:06:00 +0000

    entschaerft
    git-svn-id: https://jep.svn.sourceforge.net/svnroot/jep/trunk/jep@71 216fd33d-161a-0410-b2e1-d58788b044ed

--- website/synchronize htdocs.bat
+++ website/synchronize htdocs.bat
@@ -1,2 +1,2 @@
-call rsync -rvzt -e ssh htdocs/ nathanfunk@jep.sourceforge.net:/home/groups/j/je/jep/htdocs/
+rem call rsync -rvzt -e ssh htdocs/ nathanfunk@jep.sourceforge.net:/home/groups/j/je/jep/htdocs/
 pause
