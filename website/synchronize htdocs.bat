1ecbfc050db274e6d0cfd49fb02031891ede37e3
nathan <nathan@216fd33d-161a-0410-b2e1-d58788b044ed>
2003-01-19 04:36:41 +0000

    movig documentation to doc
    git-svn-id: https://jep.svn.sourceforge.net/svnroot/jep/trunk/jep@139 216fd33d-161a-0410-b2e1-d58788b044ed

--- website/synchronize htdocs.bat
+++
@@ -1,6 +0,0 @@
-echo -- DRY RUN -------------------
-call rsync -rvztn -e ssh htdocs/ nathanfunk@jep.sourceforge.net:/home/groups/j/je/jep/htdocs/
-echo -- ACTUAL TRANSFER -------------------
-pause
-rem call rsync -rvzt -e ssh htdocs/ nathanfunk@jep.sourceforge.net:/home/groups/j/je/jep/htdocs/
-pause
