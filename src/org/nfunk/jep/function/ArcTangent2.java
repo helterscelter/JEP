1ead882857624a7e45580b58cba83668e4869d0a
nathanfunk <nathanfunk@216fd33d-161a-0410-b2e1-d58788b044ed>
2004-09-20 00:00:14 +0000

    renamed Angle.java
    git-svn-id: https://jep.svn.sourceforge.net/svnroot/jep/trunk/jep@247 216fd33d-161a-0410-b2e1-d58788b044ed

---
+++ src/org/nfunk/jep/function/ArcTangent2.java
@@ -0,0 +1,40 @@
+/*****************************************************************************
+
+@header@
+@date@
+@copyright@
+@license@
+
+*****************************************************************************/
+
+package org.nfunk.jep.function;
+
+import java.lang.Math;
+import java.util.*;
+import org.nfunk.jep.*;
+
+public class ArcTangent2 extends PostfixMathCommand
+{
+	public ArcTangent2()
+	{
+		numberOfParameters = 2;
+	}
+	
+	public void run(Stack inStack)
+		throws ParseException 
+	{
+		checkStack(inStack);// check the stack
+		Object param2 = inStack.pop();
+		Object param1 = inStack.pop();
+		
+		if ((param1 instanceof Number) && (param2 instanceof Number))
+		{
+			double x = ((Number)param1).doubleValue();
+			double y = ((Number)param2).doubleValue();
+			inStack.push(new Double(Math.atan2(x, y)));//push the result on the inStack
+		}
+		else
+			throw new ParseException("Invalid parameter type");
+		return;
+	}
+}
