e37ea90684bed5dc3d48acaa59d6b06d9827b8ae
nathanfunk <nathanfunk@216fd33d-161a-0410-b2e1-d58788b044ed>
2004-09-26 07:17:15 +0000

    changed to appletviewer from jview
    git-svn-id: https://jep.svn.sourceforge.net/svnroot/jep/trunk/jep@263 216fd33d-161a-0410-b2e1-d58788b044ed

--- bin/FunctionPlotter.bat
+++ bin/FunctionPlotter.bat
@@ -1,27 +1,33 @@
 @echo off
 echo This batch file may not run correctly if executed from a path containing space characters.
 
-REM call jview.exe /cp:a d:\java\packages /a Evaluator.java
+if not "%OS%"=="Windows_NT" goto otherStart
 
-if not "%OS%"=="Windows_NT" goto win9xStart
-
+REM -- Windows NT & XP -----------------------------------------------
 :winNTStart
 @setlocal
-rem %~dp0 is name of current script under NT
+REM determine the location of JEP
+REM %~dp0 is name of current script under NT
 set JEP_HOME=%~dp0
 set JEP_HOME=%JEP_HOME%\..
 echo JEP_HOME = %JEP_HOME%
 
-call appletviewer.exe -J-classpath -J%JEP_HOME%\build\ "file://%JEP_HOME%\src\org\nfunk\jepexamples\FunctionPlotter.java"
-REM call jview.exe /cp:a %JEP_HOME%\build\ /a "%JEP_HOME%\src\org\nfunk\jepexamples\FunctionPlotter.java"
+REM change directories to the examples source code directory
+cd "%JEP_HOME%\src\org\nfunk\jepexamples"
+
+REM execute the applet viewer 
+appletviewer.exe -J-classpath -J%JEP_HOME%\build\ FunctionPlotter.java
+REM jview.exe /cp:a %JEP_HOME%\build\ /a "%JEP_HOME%\src\org\nfunk\jepexamples\Evaluator.java"
 @endlocal
 goto mainEnd
 
 
-:win9xStart
-echo No Windows 9x batch support yet...
+REM -- Other OS (probably Windows 9x) --------------------------------
+:otherStart
+echo This batch file only supports Windows NT, 2000 and XP supported...
 goto mainEnd
 
 
+REM -- End -----------------------------------------------------------
 :mainEnd
 pause
