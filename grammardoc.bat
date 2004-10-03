005e7e0efe1445a3238f04b9c0aef9975c838c3e
nathanfunk <nathanfunk@216fd33d-161a-0410-b2e1-d58788b044ed>
2004-10-03 23:15:03 +0000

    compile.bat probably just confuses people grammardoc.bat is now performed in ant task
    git-svn-id: https://jep.svn.sourceforge.net/svnroot/jep/trunk/jep@294 216fd33d-161a-0410-b2e1-d58788b044ed

--- grammardoc.bat
+++
@@ -1,5 +0,0 @@
-mkdir doc
-mkdir doc\grammar
-call jjdoc -OUTPUT_FILE:doc\grammar\grammar.html src\org\nfunk\jep\Parser.jj
-call jjdoc -TEXT:true -OUTPUT_FILE:doc\grammar\grammar.txt src\org\nfunk\jep\Parser.jj
-pause
