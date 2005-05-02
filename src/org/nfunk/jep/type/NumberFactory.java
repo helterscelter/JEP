66ca5d8ea354759812bdcb62b9843e3a63850d61
pfafrich <pfafrich@216fd33d-161a-0410-b2e1-d58788b044ed>
2005-05-02 20:14:38 +0000

    Added more functionality to NumberFactory. CreateNumber(double) etc. and getZero() thinking about methods in factory for basic add, multiply ops.
    git-svn-id: https://jep.svn.sourceforge.net/svnroot/jep/trunk/jep@434 216fd33d-161a-0410-b2e1-d58788b044ed

--- src/org/nfunk/jep/type/NumberFactory.java
+++ src/org/nfunk/jep/type/NumberFactory.java
@@ -9,6 +9,8 @@
 
 package org.nfunk.jep.type;
 
+import org.nfunk.jep.ParseException;
+
 /**
  * This interface can be implemented to create numbers of any object type.
  * By implementing this interface and calling the setNumberFactory() method of
@@ -21,8 +23,29 @@ public interface NumberFactory {
 	 * Creates a number object and initializes its value.
 	 * @param value The initial value of the number as a string.
 	 */
-	public Object createNumber(String value);
-	///** Creates a number with given double value. */
-	//public Object createNumber(double value);
-	//public Object createNumber(Double value);
+	public Object createNumber(String value) throws ParseException;
+	/** Creates a number object with given double value. */
+	public Object createNumber(double value) throws ParseException;
+	/** Create a number object with given int value */
+	public Object createNumber(int value) throws ParseException;
+	/** Create a number object with given short value */
+	public Object createNumber(short value) throws ParseException;
+	/** Create a number object with given float value */
+	public Object createNumber(float value) throws ParseException;
+	/** Create a number object with given boolean value */
+	public Object createNumber(boolean value) throws ParseException;
+	/** Creates a number object from a class implementing Number,
+	 * May actually just return the class. */
+	public Object createNumber(Number value) throws ParseException;
+	/** Create a number object with given Complex value */
+	public Object createNumber(Complex value) throws ParseException;
+	
+	/** Return an object representing ZERO the additive identity. */
+	public Object getZero();
+	/** Return an object representing ONE the multiplicative identity. */
+	public Object getOne();
+	/** Return an object representing ZERO-ONE. */
+	public Object getMinusOne();
+	/** Return an object representing ONE+ONE. */
+	public Object getTwo();
 }
