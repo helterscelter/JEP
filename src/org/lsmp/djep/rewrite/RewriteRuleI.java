49bdaa3d5f503e7a2cba257eb9fe75987fb09647
pfafrich <pfafrich@216fd33d-161a-0410-b2e1-d58788b044ed>
2005-06-07 22:37:14 +0000

    Doc update to emphesis experimental nature.
    git-svn-id: https://jep.svn.sourceforge.net/svnroot/jep/trunk/jep@442 216fd33d-161a-0410-b2e1-d58788b044ed

--- src/org/lsmp/djep/rewrite/RewriteRuleI.java
+++ src/org/lsmp/djep/rewrite/RewriteRuleI.java
@@ -12,6 +12,8 @@ import org.nfunk.jep.*;
  * Created on 01-Oct-2004
  */
 public interface RewriteRuleI {
+    /** Returns true if node needs to be rewritten, according to this rule. */
 	public boolean test(ASTFunNode node,Node children[]);
+	/** Rewrites the node */
 	public Node apply(ASTFunNode node,Node children[]) throws ParseException;
 }
