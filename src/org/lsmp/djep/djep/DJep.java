2637adc332ff2e2a3286c7e20d613d65367825a4
pfafrich <pfafrich@216fd33d-161a-0410-b2e1-d58788b044ed>
2005-07-28 10:25:46 +0000

    Re structured classes so the standard diff rules are added in DJep rather than DifferentationVisitor.
    git-svn-id: https://jep.svn.sourceforge.net/svnroot/jep/trunk/jep@445 216fd33d-161a-0410-b2e1-d58788b044ed

--- src/org/lsmp/djep/djep/DJep.java
+++ src/org/lsmp/djep/djep/DJep.java
@@ -6,6 +6,13 @@
  * <a href="http://creativecommons.org/licenses/by-nc-sa/1.0">License</a>
  */
 package org.lsmp.djep.djep;
+import org.lsmp.djep.djep.diffRules.DivideDiffRule;
+import org.lsmp.djep.djep.diffRules.MacroDiffRules;
+import org.lsmp.djep.djep.diffRules.MacroFunctionDiffRules;
+import org.lsmp.djep.djep.diffRules.MultiplyDiffRule;
+import org.lsmp.djep.djep.diffRules.PassThroughDiffRule;
+import org.lsmp.djep.djep.diffRules.PowerDiffRule;
+import org.lsmp.djep.djep.diffRules.SubtractDiffRule;
 import org.lsmp.djep.xjep.*;
 import org.nfunk.jep.*;
 /**
@@ -40,6 +47,15 @@ public class DJep extends XJep {
 		this.symTab = new DSymbolTable(new DVariableFactory());
 
 		addFunction("diff",new Diff());
+		
+		//addDiffRule(new AdditionDiffRule("+"));
+		addDiffRule(new PassThroughDiffRule("+",this.getOperatorSet().getAdd().getPFMC()));
+		addDiffRule(new SubtractDiffRule("-"));
+		addDiffRule(new MultiplyDiffRule("*"));
+		addDiffRule(new DivideDiffRule("/"));
+		addDiffRule(new PowerDiffRule("^"));
+		addDiffRule(new PassThroughDiffRule("UMinus",this.getOperatorSet().getUMinus().getPFMC()));
+
 	}
 	/**
 	 * Differentiate an equation with respect to a variable.
@@ -57,6 +73,7 @@ public class DJep extends XJep {
 	{
 		super(j);
 		this.dv=j.dv;
+		
 	}
 
 	public XJep newInstance()
@@ -75,12 +92,97 @@ public class DJep extends XJep {
 	 * Returns the visitor used for differentiation. Allows more advanced functions.
 	 */
 	public DifferentiationVisitor getDifferentationVisitor() { return dv; }
-	/** 
-	 * Adds the standard set of differentation rules.
-	 * @return false if there was a parse error. 
-	 */ 
-	public boolean addStandardDiffRules() { return dv.addStandardDiffRules(); }
-	//public DPrintVisitor getDPrintVisitor() { return (DPrintVisitor) pv; }
+	
+	 /** 
+	   * Adds the standard set of differentation rules. 
+	   * Corresponds to all standard functions in the JEP plus a few more.
+	   * <pre>
+	   * sin,cos,tan,asin,acos,atan,sinh,cosh,tanh,asinh,acosh,atanh
+	   * sqrt,log,ln,abs,angle
+	   * sum,im,re are handled separately.
+	   * rand and mod currently un-handled
+	   * 
+	   * Also adds rules for functions not in JEP function list:
+	   * 	sec,cosec,cot,exp,pow,sgn 
+	   * 
+	   * TODO include if, min, max, sgn
+	   * </pre>
+	   * @return false on error
+	   */
+	  public boolean addStandardDiffRules()
+	  {
+	  	try
+	  	{
+	  		addDiffRule(new MacroDiffRules(this,"sin","cos(x)"));
+			addDiffRule(new MacroDiffRules(this,"cos","-sin(x)")); 	
+			addDiffRule(new MacroDiffRules(this,"tan","1/((cos(x))^2)"));
+
+			
+			addDiffRule(new MacroDiffRules(this,"sec","sec(x) * tan(x)"));
+			addDiffRule(new MacroDiffRules(this,"cosec","-cosec(x) * cot(x)"));
+			addDiffRule(new MacroDiffRules(this,"cot","-(cosec(x))^2"));
+				
+			addDiffRule(new MacroDiffRules(this,"asin","1/(sqrt(1-x^2))"));
+			addDiffRule(new MacroDiffRules(this,"acos","-1/(sqrt(1-x^2))"));
+			addDiffRule(new MacroDiffRules(this,"atan","1/(1+x^2)"));
+
+			addDiffRule(new MacroDiffRules(this,"sinh","cosh(x)"));
+			addDiffRule(new MacroDiffRules(this,"cosh","sinh(x)"));
+			addDiffRule(new MacroDiffRules(this,"tanh","1-(tanh(x))^2"));
+
+			addDiffRule(new MacroDiffRules(this,"asinh","1/(sqrt(1+x^2))"));
+			addDiffRule(new MacroDiffRules(this,"acosh","1/(sqrt(x^2-1))"));
+			addDiffRule(new MacroDiffRules(this,"atanh","1/(1-x^2)"));
+
+			addDiffRule(new MacroDiffRules(this,"sqrt","1/(2 (sqrt(x)))"));
+			
+			addDiffRule(new MacroDiffRules(this,"exp","exp(x)"));
+//			this.addFunction("pow",new Pow());
+//			addDiffRule(new MacroDiffRules(this,"pow","y*(pow(x,y-1))","(ln(x)) (pow(x,y))"));
+			addDiffRule(new MacroDiffRules(this,"ln","1/x"));
+			addDiffRule(new MacroDiffRules(this,"log",	// -> (1/ln(10)) /x = log(e) / x but don't know if e exists
+				this.getNodeFactory().buildOperatorNode(this.getOperatorSet().getDivide(),
+					this.getNodeFactory().buildConstantNode(
+						this.getTreeUtils().getNumber(1/Math.log(10.0))),
+					this.getNodeFactory().buildVariableNode(this.getSymbolTable().makeVarIfNeeded("x")))));
+			// TODO problems here with using a global variable (x) in an essentially local context
+			addDiffRule(new MacroDiffRules(this,"abs","abs(x)/x"));
+			addDiffRule(new MacroDiffRules(this,"atan2","-y/(x^2+y^2)","x/(x^2+y^2)"));
+			addDiffRule(new MacroDiffRules(this,"mod","1","0"));
+			addDiffRule(new PassThroughDiffRule(this,"sum"));
+			addDiffRule(new PassThroughDiffRule(this,"re"));
+			addDiffRule(new PassThroughDiffRule(this,"im"));
+			addDiffRule(new PassThroughDiffRule(this,"rand"));
+			
+			MacroFunction cmplx = (MacroFunction) this.getFunctionTable().get("macrocomplex");
+			if(cmplx!=null)
+			    addDiffRule(new MacroFunctionDiffRules(this,cmplx));
+			
+	/*		addDiffRule(new PassThroughDiffRule("\"<\"",this.getOperatorSet().getLT().getPFMC()));
+			addDiffRule(new PassThroughDiffRule("\">\"",new Comparative(1)));
+			addDiffRule(new PassThroughDiffRule("\"<=\"",new Comparative(2)));
+			addDiffRule(new PassThroughDiffRule("\">=\"",new Comparative(3)));
+			addDiffRule(new PassThroughDiffRule("\"!=\"",new Comparative(4)));
+			addDiffRule(new PassThroughDiffRule("\"==\"",new Comparative(5)));
+	*/		
+//			addDiffRule(new DiffDiffRule(this,"diff"));
+			// TODO do we want to add eval here?
+//			addDiffRule(new EvalDiffRule(this,"eval",eval));
+			
+			//addDiffRule(new PassThroughDiffRule("\"&&\""));
+			//addDiffRule(new PassThroughDiffRule("\"||\""));
+			//addDiffRule(new PassThroughDiffRule("\"!\""));
+			
+			// also consider if, min, max, sgn, dot, cross, 
+			//addDiffRule(new MacroDiffRules(this,"sgn","0"));
+			return true;
+	  	}
+	  	catch(ParseException e)
+	  	{
+	  		System.err.println(e.getMessage());
+	  		return false;
+	  	}
+	  }
 
 	/**
 	 * Adds a rule with instruction on how to differentiate a function.
