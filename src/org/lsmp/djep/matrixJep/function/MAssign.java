11b020bee07fb74f844a438535a08a53b58feabe
nathanfunk <nathanfunk@216fd33d-161a-0410-b2e1-d58788b044ed>
2006-02-19 01:46:55 +0000

    removed wrong copyright reference
    git-svn-id: https://jep.svn.sourceforge.net/svnroot/jep/trunk/jep@482 216fd33d-161a-0410-b2e1-d58788b044ed

--- src/org/lsmp/djep/matrixJep/function/MAssign.java
+++ src/org/lsmp/djep/matrixJep/function/MAssign.java
@@ -1,11 +1,3 @@
-/*****************************************************************************
-
-JEP - Java Math Expression Parser 2.24
-	  December 30 2002
-	  (c) Copyright 2002, Nathan Funk
-	  See LICENSE.txt for license information.
-
-*****************************************************************************/
 package org.lsmp.djep.matrixJep.function;
 
 import java.util.*;
