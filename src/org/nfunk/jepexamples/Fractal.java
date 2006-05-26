08dcf0d76b803a8fa3e4229c4637b7da71d83329
nathanfunk <nathanfunk@216fd33d-161a-0410-b2e1-d58788b044ed>
2006-05-26 05:06:10 +0000

    Removed unused variable
    git-svn-id: https://jep.svn.sourceforge.net/svnroot/jep/trunk/jep@521 216fd33d-161a-0410-b2e1-d58788b044ed

--- src/org/nfunk/jepexamples/Fractal.java
+++ src/org/nfunk/jepexamples/Fractal.java
@@ -21,7 +21,7 @@ import java.awt.event.*;
 public class Fractal extends Applet implements ActionListener {
 	private static final long serialVersionUID = -1825231934586941116L;
 	private TextField exprField, itField;
-	private Button button, button2;
+	private Button button;
 	private FractalCanvas complexCanvas;
 
 
